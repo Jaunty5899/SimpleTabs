de085bb57b18bf6693ef634b8cf72cd159db17b7
Jaunty <jaunty5899@gmail.com>
2024-12-23 13:27:37 +0530

    tab switches done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import { useState } from "react";
 import "./App.css";
 
+const tabs = [
+  { tabName: "First Tab" },
+  { tabName: "Second Tab" },
+  { tabName: "Third Tab" },
+  { tabName: "Fourth Tab" },
+];
+
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <div className="container">
+      <ul>
+        {tabs.map((e) => {
+          return <li>{e.tabName}</li>;
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
